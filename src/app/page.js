f8400672c0407fca3486fde1ccb96dcfcdb68e0a
supermeng <supermeng@redfin.tech>
2024-04-24 19:53:52 +0800

    fix layout width

--- src/app/page.js
+++ src/app/page.js
@@ -148,8 +148,8 @@ export default function Home() {
         <div className='flex flex-col w-full'>
           <Carousel carouselItems={carousel} />
           <div style={{ display: 'flex', justifyContent: 'center' }}>
-          {/* md:mx-20 mx-2.5 */}
-            <div className='pt-4 container lg:w-[80%] w-[100%]'>
+          {/* md:mx-20 mx-2.5  lg:w-[80%]*/}
+            <div className='pt-4 container  w-[100%]'>
               {yunying != [] &&
                 yunying?.map((yy, idx) => {
                   return (
