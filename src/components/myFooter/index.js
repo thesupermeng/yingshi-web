b7d554b0c931cc719aca9a6847882d0649639517
supermeng <supermeng@redfin.tech>
2024-04-24 17:19:51 +0800

    temporary hide 我的

--- src/components/myFooter/index.js
+++ src/components/myFooter/index.js
@@ -102,7 +102,7 @@ const MyFooter = () => {
           </div>
           <div>播单</div>
         </div>
-        <div
+        {/* <div
           className='col flex-col d-flex justify-center align-center items-center'
           onClick={() => {
             handleClick(990);
@@ -117,7 +117,7 @@ const MyFooter = () => {
             />
           </div>
           <div>我的</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
