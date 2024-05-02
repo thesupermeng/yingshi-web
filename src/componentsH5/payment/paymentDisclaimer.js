d08f49e8aa1a6e116c2e9020f382dcc1b7c1e9ab
carl <carl@redfin.tech>
2024-05-02 17:27:22 +0800

    added onclick handlers

--- src/componentsH5/payment/paymentDisclaimer.js
+++ src/componentsH5/payment/paymentDisclaimer.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
 export default function PaymentDisclaimer({className}) {
+
+  const handleClickPrivacy = () => {
+    console.log('Privacy clicked')
+  }
+
+  const handleClickService = () => {
+    console.log('Service clicked')
+  }
+
+  const handleClickAutoRenew = () => {
+    console.log('Renewal clicked')
+  }
+
+  const handleClickEmail = () => {
+    console.log('Email clicked')
+  }
+
   return (
     <div className={`text-[12px] text-[#9C9C9C] text-center ${className}`}>
       <p>
-        <span>隐私协议</span>&nbsp;|&nbsp;
-        <span>用户服务协议</span>&nbsp;|&nbsp;
-        <span>自动续费协议</span>
+        <span onClick={handleClickPrivacy}>隐私协议</span>&nbsp;|&nbsp;
+        <span onClick={handleClickService}>用户服务协议</span>&nbsp;|&nbsp;
+        <span onClick={handleClickAutoRenew}>自动续费协议</span>
       </p>
       <p>
         如遇支付问题，请联系
-        <span><u>contact.movie9@gmail.com</u></span>
+        <span onClick={handleClickEmail}><u>contact.movie9@gmail.com</u></span>
       </p>
     </div>
 
--- src/componentsH5/payment/paymentPurchaseButton.js
+++ src/componentsH5/payment/paymentPurchaseButton.js
@@ -1,9 +1,14 @@
 import {Button} from '@material-tailwind/react';
 import styles from './styles.module.css'
 
-export default function PaymentPurchaseButton ({className}) {
+export default function PaymentPurchaseButton ({productInfo, className}) {
+
+  const handleOnPurchase = () => {
+    console.log('Purchase clicked')
+  }
+
   return (
-    <Button className={`${styles.payment_button} ${className}`} >
+    <Button className={`${styles.payment_button} ${className}`} onClick={handleOnPurchase}>
       <span className={'text-[#1D2023] text-[19px] font-semibold'}>
         立即解锁 - 总额¥9
       </span>
