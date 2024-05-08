6d3510a759e5c8d995eb19a1f394aace8a7731b0
carl <carl@redfin.tech>
2024-05-08 13:02:23 +0800

    aha iframe chg back to staging

--- src/app/myprofile/h5Page.js
+++ src/app/myprofile/h5Page.js
@@ -189,7 +189,7 @@ export default function H5Page({params}) {
         <div style={{background: '#1D2023', borderRadius: '12px', marginBottom: '16px'}}>
           <iframe
             className={'h-[74px] w-full'}
-            src={`https://iframe-m.aha888.vip/user/wallet?authToken=${ahaToken}`}
+            src={`https://iframe-m.ggsimida.com/user/wallet?authToken=${ahaToken}`}
             scrolling={'no'}
           />
         </div>
