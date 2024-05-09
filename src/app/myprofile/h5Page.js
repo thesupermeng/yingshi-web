ecab010c94e1c478ad964ea4e43ec886dd8ac312
carl <carl@redfin.tech>
2024-05-09 17:08:36 +0800

    update aha domain

--- src/app/myprofile/h5Page.js
+++ src/app/myprofile/h5Page.js
@@ -193,7 +193,7 @@ export default function H5Page({params}) {
         <div style={{background: '#1D2023', borderRadius: '12px', marginBottom: '16px'}}>
           <iframe
             className={'h-[74px] w-full rounded-[12px]'}
-            src={`https://iframe-m.aha888.vip/user/wallet?authToken=${ahaToken}`}
+            src={`https://iframe-h5.aha666.site/user/wallet?authToken=${ahaToken}`}
             scrolling={'no'}
           />
         </div>
--- src/app/sport/[[...slug]]/page.js
+++ src/app/sport/[[...slug]]/page.js
@@ -9,13 +9,13 @@ export default function Page ({params}) {
       <div className='desktop w-full flex flex-1 flex-col'>
         <iframe
           className={'flex-1'}
-          src={`https://iframe.aha888.vip/${redirect}`}
+          src={`https://iframe-web.aha666.site/${redirect}`}
         />
       </div>
       <div className="mobile w-screen h-full">
         <iframe
           className={'w-full h-full'}
-          src={`https://iframe-m.aha888.vip/${redirect}`}
+          src={`https://iframe-h5.aha666.site/${redirect}`}
         />
       </div>
     </>
