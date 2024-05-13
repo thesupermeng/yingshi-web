93b145e2ac94635ce3202003bfded9c1ff5c2486
carl <carl@redfin.tech>
2024-05-13 15:20:52 +0800

    add payment to web

--- src/components/payment/paymentModal.js
+++ src/components/payment/paymentModal.js
@@ -21,8 +21,8 @@ export default function PaymentModal({open, handler}) {
   }, [])
 
   return (
-    <Dialog open={open} handler={handler} className={'w-[500px] bg-[#121212] rounded-[28px] p-[38px] outline-none'}
-            size={'md'}>
+    <Dialog open={open} handler={handler} className={'bg-[#121212] rounded-[28px] p-[38px] outline-none'}
+            size={'sm'}>
       <DialogBody className={'p-0 w-full h-full'}>
         <PaymentHeader/>
         <PaymentCountdown className={'mt-[28px]'}/>
