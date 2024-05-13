8aac13394f517c2b2a83ad5d240fafafe098a10a
supermeng <supermeng@redfin.tech>
2024-05-13 16:33:20 +0800

    片源的《》功能操作不了

--- src/components/vod/vodSourceList.js
+++ src/components/vod/vodSourceList.js
@@ -6,51 +6,77 @@ import {
 } from "@/asset/icons";
 
 import styles from './style.module.css';
+import { useState, useRef } from 'react';
 
 export const VodSourceList = ({
   vodSources,
   vodSourceSelected,
   onSelectSource,
 }) => {
-  return <div className="flex justify-between space-x-2">
+  const containerRef = useRef(null);
 
-    {vodSources?.length > 3 &&
-      <div className="lg:flex hidden">
-        <div id="control-left" name="control" className={styles.arrowCard}>
-          <Image
-            src={ArrowLeftIcon}
-            alt="Icon"
-          />
-        </div>
-      </div>
+  const handleScroll = (scrollOffset) => {
+    if (containerRef.current) {
+      const newScrollLeft = containerRef.current.scrollLeft + scrollOffset;
+      containerRef.current.scrollTo({ left: newScrollLeft, behavior: 'smooth' });
     }
+  };
 
-    <ul className="flex flex-1 overflow-x-scroll space-x-2 hide-scrollbar">
-      {vodSources?.map((source) => (
-        <li key={`key-vodSource-${source.source_id}`} id={`vodSource-${source.source_id}`} className={`${styles.radioOptionCard} ${vodSourceSelected.source_id === source.source_id ? styles.selectedOptionCard : styles.unselectedOptionCard}`} onClick={() => onSelectSource(source)}>
-          <div htmlFor={`vodSource-${source.source_id}`} className={`flex flex-row space-x-1 items-center`}>
-            <div className="w-6 h-6 flex justify-center items-center">
-              <Image
-                style={{ paddingRight: '2px' }}
-                src={PlaySourceIcon}
-                alt="Icon"
-              />
-            </div>
-            <span className="whitespace-nowrap text-sm">{source.source_name}</span>
+  return (
+    <div className="flex justify-between space-x-2">
+      {vodSources?.length > 3 && (
+        <div className="lg:flex hidden">
+          <div
+            id="control-left"
+            name="control"
+            className={styles.arrowCard}
+            onClick={() => handleScroll(-100)} // Adjust scroll offset as needed
+          >
+            <Image src={ArrowLeftIcon} alt="Icon" />
           </div>
-        </li>
-      ))}
-    </ul>
-
-    {vodSources?.length > 3 &&
-      <div className="lg:flex hidden">
-        <div id="control-left" name="control" className={styles.arrowCard}>
-          <Image
-            src={ArrowRightIcon}
-            alt="Icon"
-          />
         </div>
-      </div>
-    }
-  </div>
-}
+      )}
+
+      <ul
+        ref={containerRef} // Use ref to access the container
+        className="flex flex-1 overflow-x-scroll space-x-2 hide-scrollbar"
+      >
+        {vodSources?.map((source) => (
+          <li
+            key={`key-vodSource-${source.source_id}`}
+            id={`vodSource-${source.source_id}`}
+            className={`${styles.radioOptionCard} ${
+              vodSourceSelected.source_id === source.source_id
+                ? styles.selectedOptionCard
+                : styles.unselectedOptionCard
+            }`}
+            onClick={() => onSelectSource(source)}
+          >
+            <div
+              htmlFor={`vodSource-${source.source_id}`}
+              className="flex flex-row space-x-1 items-center"
+            >
+              <div className="w-6 h-6 flex justify-center items-center">
+                <Image style={{ paddingRight: '2px' }} src={PlaySourceIcon} alt="Icon" />
+              </div>
+              <span className="whitespace-nowrap text-sm">{source.source_name}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      {vodSources?.length > 3 && (
+        <div className="lg:flex hidden">
+          <div
+            id="control-right"
+            name="control"
+            className={styles.arrowCard}
+            onClick={() => handleScroll(100)} // Adjust scroll offset as needed
+          >
+            <Image src={ArrowRightIcon} alt="Icon" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
