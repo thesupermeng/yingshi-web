074232c570daf1e12e5dbab443b884fdffe861b4
supermeng <supermeng@redfin.tech>
2024-06-05 18:04:24 +0800

    add back  show bottom tab for aha tab

--- src/components/myFooter/index.js
+++ src/components/myFooter/index.js
@@ -60,7 +60,7 @@ const MyFooter = () => {
     pathname.startsWith('/enterpin') ||
     pathname.startsWith('/privacy') ||
     pathname.startsWith('/service') ||
-    pathname.startsWith('/sport') ||
+   // pathname.startsWith('/sport') ||
     pathname.startsWith('/myprofile/watchHistory') ||
     pathname.startsWith('/myprofile/userCenter') ||
     pathname.startsWith('/myprofile/feedback') ||
