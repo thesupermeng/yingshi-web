bf4e5fbb8467ab8c8068d2454ba1524db1845aa6
supermeng <supermeng@redfin.tech>
2024-06-07 19:57:35 +0800

    solve merge issue

--- src/app/login/otp/page.js
+++ src/app/login/otp/page.js
@@ -143,6 +143,7 @@ export default function OTP() {
                             dispatch(setYingshiUserLoginParam({ ...loginParam, success: true }))
                             dispatch(setYingshiUserToken(res.data.access_token))
                             dispatch(setAhaToken(res.data.aha_token))
+                            localStorage.setItem('AuthToken' ,res.data.aha_token )
                             if (res.code === 0) {
 
 
@@ -185,6 +186,7 @@ export default function OTP() {
                             dispatch(setYingshiUserLoginParam({ ...loginParam, success: true }))
                             dispatch(setYingshiUserToken(res.data.access_token))
                             dispatch(setAhaToken(res.data.aha_token))
+                            localStorage.setItem('AuthToken' ,res.data.aha_token )
                             if (res.code === 0) {
                                 //signup
                                 // router.push('/myprofile')
