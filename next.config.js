ea0f16a8c70ea57125ae43fb374fc59ed63823e6
supermeng <supermeng@redfin.tech>
2024-06-12 22:25:00 +0800

    force https

--- next.config.js
+++ next.config.js
@@ -2,18 +2,33 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'localhost' }],
+        destination: '/:path*',
+        permanent: false,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: '^(?!localhost).*$' }],
+        destination: 'https://:host/:path*',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: 'https://api.yingshi.tv/:path*',
       },
-    ]
+    ];
   },
   i18n,
   transpilePackages: ['crypto-js'],
   images: {
-    // unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,6 +45,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ['crypto-js'],
 };
+
 module.exports = nextConfig;
