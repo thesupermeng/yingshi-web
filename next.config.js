2acef90a9795be3ca0094fccde1b6429319aea30
supermeng <supermeng@redfin.tech>
2024-06-12 22:30:04 +0800

    solve Site is vulnerable to click jax

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
