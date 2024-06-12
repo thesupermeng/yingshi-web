84c993bd19eefd7a1b4abe3b7178696385065046
supermeng <supermeng@redfin.tech>
2024-06-12 23:11:18 +0800

    pako

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
+// server.js
+const { createServer } = require('http');
 const next = require('next');
-const express = require('express');
-const sslRedirect = require('heroku-ssl-redirect').default; // to make it work with 'require' keyword.
+const absoluteUrl = require('next-absolute-url').default;
+const { parse } = require('url');
 
-const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app.prepare().then(() => createServer((req, res) => {
+	const { url } = req;
+	const { protocol, host } = absoluteUrl(req);
 
-  // Express's middleware to automatically redirect to 'https'.
-  server.use(sslRedirect());
+	if (!dev && protocol === 'http:') {
+		res.writeHead(301, {
+			Location: `https://${host}${url}`
+		});
+		res.end();
+		return {};
+	}
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, err => {
-    if (err) throw err;
-
-    console.log(`Server starts on ${PORT}.`);
-  });
-});
+	const parsedUrl = parse(url, true);
+	return handle(req, res, parsedUrl);
+}).listen(3000, (err) => {
+	if (err) throw err;
+	console.log('> Sever is running!');
+}));
