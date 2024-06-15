7ef6c36e07905da8ba7b2427ed5030d6a80f2c6b
supermeng <supermeng@redfin.tech>
2024-06-15 11:26:14 +0800

    fix purchase break into 2 line

--- src/components/myprofile/VipCard.js
+++ src/components/myprofile/VipCard.js
@@ -6,15 +6,15 @@ import {Button} from '@material-tailwind/react';
 export default function VipCard({onClick}) {
   return (
     <div className={'w-full'}>
-      <div className={'flex h-[58px] bg-[#D1AC7D] rounded-lg p-1.5 items-center gap-2'}>
+      <div className={'flex h-[58px] bg-[#D1AC7D] rounded-lg p-1.5 items-center gap-1'}>
         <div className={'w-12 h-12 flex justify-center items-center'}>
           <Image src={VipBlackIcon} alt={'VIP icon'}/>
         </div>
-        <div className={'flex flex-col flex-1'}>
+        <div className={'flex flex-col flex-1 pl-1'}>
           <span className={'text-[15px] text-[#1D2023] font-semibold'}>成为VIP</span>
           <span className={'text-[13px] text-[#1D2023]'}>去广告 看完整视频</span>
         </div>
-        <Button className={'h-12 w-26 rounded-[10px] bg-[#1D2023] flex items-center justify-center'} onClick={onClick}>
+        <Button className={'h-12 w-22 rounded-[10px] bg-[#1D2023] flex items-center justify-center'} onClick={onClick}>
           <span className={'text-[#D1AC7D] text-[15px] font-semibold'}>立即购买</span>
         </Button>
       </div>
