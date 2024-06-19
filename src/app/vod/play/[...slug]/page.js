2c753df4af912d82307cc28d59ae21a15fef211e
elena <elena@redfin.tech>
2024-06-19 11:15:44 +0800

    dynamically set metadata based on vod name

--- src/app/vod/play/[...slug]/page.js
+++ src/app/vod/play/[...slug]/page.js
@@ -1,26 +1,27 @@
-import { PlayVod } from './playVod';
+import {PlayVod} from './playVod';
 import {YingshiApi} from '@/util/YingshiApi';
 import {URL_YINGSHI_VOD} from '@/config/yingshiUrl';
 import {redirect} from 'next/navigation';
 import {Config} from '@/util/config';
 import {PlayXVod} from '@/app/vod/play/[...slug]/playXVod';
 
+export async function generateMetadata({ params }) {
+  const {vodId, tId, nId} = generateFilterParams(params.slug);
+
+  const vod = nId != 9999 ? await getVod(vodId, tId) : await getXVod(vodId, tId);
+
+  if (vod && vod.List) {
+    const title = `${vod.List[0].vod_name}在线观看 - 鲨鱼影视-海量高清视频免费在线观看`
+    return {
+      title: title,
+      description: vod.List[0].vod_content
+    };
+  }
+}
+
 export default async function Page({ params }) {
   const path = params.slug;
-  const filterParams = {}
-  for (let i = 0; i < path.length; i+=2) {
-    filterParams[path[i]] = path[i+1];
-  }
-
-  if (!filterParams.id || !filterParams.sid || !filterParams.nid) {
-    redirect('/404');
-  }
-
-  const vodId = filterParams.id;
-  const tId = filterParams.sid;
-  const nId = filterParams.nid;
-  const sourceId = parseInt(filterParams.source);
-
+  const {vodId, tId, nId, sourceId} = generateFilterParams(path);
   const vod = nId != 9999 ? await getVod(vodId, tId) : await getXVod(vodId, tId);
 
   if (!vod || !vod.List) {
@@ -54,6 +55,24 @@ export default async function Page({ params }) {
       popularList={popularList}
     />
   );
+}
+
+function generateFilterParams(path) {
+  const filterParams = {}
+  for (let i = 0; i < path.length; i+=2) {
+    filterParams[path[i]] = path[i+1];
+  }
+
+  if (!filterParams.id || !filterParams.sid || !filterParams.nid) {
+    redirect('/404');
+  }
+
+  return {
+    vodId: filterParams.id,
+    tId: filterParams.sid,
+    nId: filterParams.nid,
+    sourceId: parseInt(filterParams.source)
+  }
 }
 
 async function getVod(vodId, tId) {
