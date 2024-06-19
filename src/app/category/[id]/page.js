457976e62cae6b773ed343c9340def3fd249b1fb
elena <elena@redfin.tech>
2024-06-19 15:56:34 +0800

    fix --> header 的 menu 按了 好像没有反应

--- src/app/category/[id]/page.js
+++ src/app/category/[id]/page.js
@@ -1,9 +1,29 @@
-import { isWeb } from '@/util/common';
 import React from 'react';
-import WebPage from './WebPage';
-import H5Page from './H5Page';
+import {redirect} from 'next/navigation';
+import Home from '@/app/page';
 
-export default function Page() {
-  // return isWeb() ? <WebPage /> : <H5Page />;
-  return <H5Page />;
+export default function Page({ params }) {
+  const id = params.id;
+  const headerId = [1, 2, 3, 4, 5, 6, 7, 99];
+  const isInteger = (str) => {
+    return /^\d+$/.test(str);
+  };
+
+  if (isInteger(id)) {
+    const intId = parseInt(id);
+    if (intId === 0) {
+      redirect('/');
+    } else if (intId === 998) {
+      redirect('/topic/index/page');
+    } else if (intId === 999) {
+      redirect('/vod/show/by/time/id/1')
+    } else if (headerId.includes(intId)) {
+      // No routing needed, return to render Home component below
+      return <Home category={parseInt(id)} />;
+    } else {
+      redirect('/404');
+    }
+  } else {
+    redirect('/404');
+  }
 }
