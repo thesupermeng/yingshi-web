59b10cc231a8e0115ff2724854437daeac55375a
kelvin <kelvin@redfin.tech>
2024-06-20 20:55:10 +0800

    done -> change topic from ssr -> csr

--- src/app/topic/index/page/page.js
+++ src/app/topic/index/page/page.js
@@ -1,28 +1,49 @@
+'use client'
 import { URL_YINGSHI_VOD } from '@/config/yingshiUrl';
 import { YingshiApi } from '@/util/YingshiApi';
-import {Topic} from '@/app/topic/index/page/topic';
+import { Topic } from '@/app/topic/index/page/topic';
+import { useEffect, useState } from 'react';
+import { LoadingPage } from '@/components/loading';
 
-export default async function Page() {
-  const topicList = await getTopicListApi();
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [topicList, setTopicList] = useState(null);
 
-  return(
-    <Topic topics={topicList} />
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTopicListApi().then((result) => {
+      setTopicList(result);
+      setIsLoading(false);
+    })
+  }, []);
+  // const topicList = await getTopicListApi();
+
+  return (
+    <>
+      {isLoading
+        ? <div>
+          <LoadingPage full={false} />
+        </div>
+        : <Topic topics={topicList} />
+      }
+    </>
   );
 }
 
 async function getTopicListApi() {
   return YingshiApi(
-      URL_YINGSHI_VOD.playlistGetTopic + '?limit=18&page=1',
-      {},
-      {
-        method: 'GET',
-        noToken: true,
-        extraOptions: {
-          next: {
-            cache: 'force-cache',
-            revalidate: 3600
-          }
+    URL_YINGSHI_VOD.playlistGetTopic + '?limit=18&page=1',
+    {},
+    {
+      method: 'GET',
+      noToken: true,
+      extraOptions: {
+        next: {
+          cache: 'force-cache',
+          revalidate: 3600
         }
       }
+    }
   );
 }
