c5d49e4934c786cb6705961ac421f5feeb224007
supermeng <supermeng@redfin.tech>
2024-06-21 13:31:43 +0800

    change match pattern for header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -360,7 +360,7 @@ const Header = () => {
     } else if (pathname.startsWith('/vod/play/')) {
       setSelectedId(-1);
     } else {
-      const match = pathname.match(/\/category\/(\w+)/);
+      const match = pathname.match(/\/index\/type\/id\/(\d+)/);
       if (match) {
         setSelectedId(parseInt(match[1]));
       } else {
