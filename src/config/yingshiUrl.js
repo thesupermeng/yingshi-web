db5c119f95b91988acedb00b666baa1a6dbaca1b
Ooi Liang Zhi <liang.ooi@redfin.tech>
2024-06-25 15:52:06 +0800

    瘦身API

--- src/config/yingshiUrl.js
+++ src/config/yingshiUrl.js
@@ -13,7 +13,7 @@ export const URL_YINGSHI_VOD = {
   homeGetPages: 'page/v4.5/typepage',
   getVodDetails: 'vod/v3/vod',
   playlistGetTopic: 'topic/v1/topic',
-  playlistGetTopicDetail: 'topic/v1/topic/detail',
+  playlistGetTopicDetail: 'topic/v2/topic/detail',
   topTenList: 'topic/v1/topic/hot',
   searchingList: 'vod/v3/vod',
   searchingListSlim: 'vod/v1/vod/similar',
