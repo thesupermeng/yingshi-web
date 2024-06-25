83b7a36992fb143e8e3117964a8179cefc470837
supermeng <supermeng@redfin.tech>
2024-06-25 18:41:40 +0800

    update seo

--- src/app/index/type/id/[id]/page.js
+++ src/app/index/type/id/[id]/page.js
@@ -2,6 +2,51 @@ import React from 'react';
 import {redirect} from 'next/navigation';
 import Home from '@/app/page';
 
+
+export async function generateMetadata({ params }) {
+  console.log(params)
+  let  id  = params.id;
+  
+  let title = '';
+
+  switch (id) {
+    case '1':
+      title = '电视剧';
+      break;
+    case '2':
+      title = '电影';
+      break;
+    case '3':
+      title = '综艺';
+      break;
+    case '4':
+      title = '动漫';
+      break;
+    case '5':
+      title = '纪录片';
+      break;
+    case '46':
+      title = '短剧';
+      break;
+  }
+
+  return {
+    title: `推荐${title}- 影视TV-海量高清视频免费在线观看`,
+    description: '全球华人在线视频媒体平台,免费点播,免费提供最新高清的电影,电视剧,综艺,动漫,台劇,日劇,泰劇,韩剧,美剧等。',
+    keywords : [
+      '在线影院',
+      '在线观看',
+      '在线看电影',
+      '海外影院',
+      '免费电影',
+      '免费电视剧',
+      '韩剧',
+      '美剧',
+      '影视TV'
+    ]
+  }
+}
+
 export function generateStaticParams() {
   return [
     {id: '1'},
@@ -38,5 +83,17 @@ export default function Page({ params }) {
     }
   } else {
     redirect('/404');
+  }
+}
+
+
+function generateFilterParams(path) {
+  const filterParams = {}
+  for (let i = 0; i < path.length; i+=2) {
+    filterParams[path[i]] = path[i+1];
+  }
+
+  return {
+    id: parseInt(filterParams.id)
   }
 }
