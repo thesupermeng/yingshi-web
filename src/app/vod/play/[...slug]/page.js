b5e84932f704633f0ebf7db9ebd4f3b808871c55
supermeng <supermeng@redfin.tech>
2024-07-11 17:53:46 +0800

    solve crash

--- src/app/vod/play/[...slug]/page.js
+++ src/app/vod/play/[...slug]/page.js
@@ -12,10 +12,10 @@ export async function generateMetadata({ params }) {
 
   if (vod && vod.List) {
     const title = `${vod.List[0].vod_name}在线观看 - 影视TV-海量高清视频免费在线观看`;
-    const blurb = vod.List[0].vod_blurb;
-    let keywordsArray = blurb.split(' '); // Split the blurb into an array of words using space as the delimiter
+    const blurb = vod.List[0]?.vod_blurb;
+    let keywordsArray = blurb?.split(' '); // Split the blurb into an array of words using space as the delimiter
   
-    if (keywordsArray.length > 10) {
+    if (keywordsArray?.length > 10) {
       keywordsArray = keywordsArray.slice(0, 10);
     }
     return {
